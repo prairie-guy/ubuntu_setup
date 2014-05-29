83be11873257ba738486064b81de347fe6f7c178
prairie-guy <cdaniels@nandor.net>
2014-05-28 23:09:01 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -33,15 +33,15 @@ wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
 
 # git pull and install dotfiles as well
 cd $HOME
-if [ -d ./dotfiles/ ]; then
-    mv dotfiles dotfiles.old
+if [ -d ./.dotfiles/ ]; then
+    mv .dotfiles .dotfiles.old
 fi
 if [ -d .emacs.d/ ]; then
     mv .emacs.d .emacs.d~
 fi
-git clone https://github.com/prairie-guy/ubuntu_dotfiles.git
-ln -sb dotfiles/.screenrc .
-ln -sb dotfiles/.bash_profile .
-ln -sb dotfiles/.bashrc .
-ln -sb dotfiles/.bashrc_custom .
-ln -sf dotfiles/.emacs.d .
+git clone https://github.com/prairie-guy/ubuntu_dotfiles.git .dotfiles
+ln -sb .dotfiles/.screenrc .
+ln -sb .dotfiles/.bash_profile .
+ln -sb .dotfiles/.bashrc .
+ln -sb .dotfiles/.bashrc_custom .
+ln -sf .dotfiles/.emacs.d .
